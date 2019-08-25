96d7b4cc316597aabe40b8de5a7fcf861c3a1b9a
kyle <thekyle@outlook.com>
2019-08-25 13:18:08 +0900

    started profile page/imageinput/update still broken

--- src/Redux/ActionTypes.js
+++ src/Redux/ActionTypes.js
@@ -1,1 +1,2 @@
 export const ADD_USER = 'ADD_USER'
+export const UPDATE_USER = 'UPDATE_USER'
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,2 +1,3 @@
 export const home = '/'
 export const about = '/about'
+export const profile = '/profile'
