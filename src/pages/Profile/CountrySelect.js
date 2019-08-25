e2542946b95c30f5fc94ec4a6f4bd8e8ea4617be
HazardousHenkie <thekyle@hotmail.com>
2019-08-25 21:21:19 +0900

    starting to add multi select

---
+++ src/pages/Profile/CountrySelect.js
@@ -0,0 +1,260 @@
+import React from 'react'
+import clsx from 'clsx'
+import Select from 'react-select'
+import { emphasize, makeStyles, useTheme } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import NoSsr from '@material-ui/core/NoSsr'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import Chip from '@material-ui/core/Chip'
+import MenuItem from '@material-ui/core/MenuItem'
+import CancelIcon from '@material-ui/icons/Cancel'
+
+const suggestions = [
+  { label: 'Afghanistan' },
+  { label: 'Aland Islands' },
+  { label: 'Albania' },
+  { label: 'Algeria' },
+  { label: 'American Samoa' },
+  { label: 'Andorra' },
+  { label: 'Angola' },
+  { label: 'Anguilla' },
+  { label: 'Antarctica' },
+  { label: 'Antigua and Barbuda' },
+  { label: 'Argentina' },
+  { label: 'Armenia' },
+  { label: 'Aruba' },
+  { label: 'Australia' },
+  { label: 'Austria' },
+  { label: 'Azerbaijan' },
+  { label: 'Bahamas' },
+  { label: 'Bahrain' },
+  { label: 'Bangladesh' },
+  { label: 'Barbados' },
+  { label: 'Belarus' },
+  { label: 'Belgium' },
+  { label: 'Belize' },
+  { label: 'Benin' },
+  { label: 'Bermuda' },
+  { label: 'Bhutan' },
+  { label: 'Bolivia, Plurinational State of' },
+  { label: 'Bonaire, Sint Eustatius and Saba' },
+  { label: 'Bosnia and Herzegovina' },
+  { label: 'Botswana' },
+  { label: 'Bouvet Island' },
+  { label: 'Brazil' },
+  { label: 'British Indian Ocean Territory' },
+  { label: 'Brunei Darussalam' }
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label
+}))
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    height: 250,
+    minWidth: 290
+  },
+  input: {
+    display: 'flex',
+    padding: 0,
+    height: 'auto'
+  },
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+  chip: {
+    margin: theme.spacing(0.5, 0.25)
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === 'light'
+        ? theme.palette.grey[300]
+        : theme.palette.grey[700],
+      0.08
+    )
+  },
+  noOptionsMessage: {
+    padding: theme.spacing(1, 2)
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 2,
+    bottom: 6,
+    fontSize: 16
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing(1),
+    left: 0,
+    right: 0
+  },
+  divider: {
+    height: theme.spacing(2)
+  }
+}))
+
+function NoOptionsMessage(props) {
+  const { selectProps, innerProps, children } = props
+  return (
+    <Typography
+      color="textSecondary"
+      className={selectProps.classes.noOptionsMessage}
+      {...innerProps}
+    >
+      {children}
+    </Typography>
+  )
+}
+
+function inputComponent({ inputRef, ...props }) {
+  return <div ref={inputRef} {...props} />
+}
+
+function Control(props) {
+  const {
+    children,
+    innerProps,
+    innerRef,
+    selectProps: { classes, TextFieldProps }
+  } = props
+
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: classes.input,
+          ref: innerRef,
+          children,
+          ...innerProps
+        }
+      }}
+      {...TextFieldProps}
+    />
+  )
+}
+
+function Option(props) {
+  const { innerRef, isFocused, isSelected, innerProps, children } = props
+  return (
+    <MenuItem
+      ref={innerRef}
+      selected={isFocused}
+      component="div"
+      style={{
+        fontWeight: isSelected ? 500 : 400
+      }}
+      {...innerProps}
+    >
+      {children}
+    </MenuItem>
+  )
+}
+
+function Placeholder(props) {
+  const { selectProps, innerProps = {}, children } = props
+  return (
+    <Typography
+      color="textSecondary"
+      className={selectProps.classes.placeholder}
+      {...innerProps}
+    >
+      {children}
+    </Typography>
+  )
+}
+
+function ValueContainer(props) {
+  const { selectProps, children } = props
+  return <div className={selectProps.classes.valueContainer}>{children}</div>
+}
+
+function MultiValue(props) {
+  const { children, selectProps, isFocused, removeProps } = props
+  return (
+    <Chip
+      tabIndex={-1}
+      label={children}
+      className={clsx(selectProps.classes.chip, {
+        [selectProps.classes.chipFocused]: isFocused
+      })}
+      onDelete={removeProps.onClick}
+      deleteIcon={<CancelIcon {...removeProps} />}
+    />
+  )
+}
+
+function Menu(props) {
+  const { selectProps, innerProps, children } = props
+  return (
+    <Paper square className={selectProps.classes.paper} {...innerProps}>
+      {children}
+    </Paper>
+  )
+}
+
+const components = {
+  Control,
+  Menu,
+  MultiValue,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  ValueContainer
+}
+
+export default function IntegrationReactSelect() {
+  const classes = useStyles()
+  const theme = useTheme()
+  const [multi, setMulti] = React.useState(null)
+
+  function handleChangeMulti(value) {
+    setMulti(value)
+  }
+
+  const selectStyles = {
+    input: base => ({
+      ...base,
+      color: theme.palette.text.primary,
+      '& input': {
+        font: 'inherit'
+      }
+    })
+  }
+
+  return (
+    <div className={classes.root}>
+      <NoSsr>
+        <Select
+          classes={classes}
+          styles={selectStyles}
+          inputId="react-select-multiple"
+          TextFieldProps={{
+            label: 'Countries',
+            InputLabelProps: {
+              htmlFor: 'react-select-multiple',
+              shrink: true
+            }
+          }}
+          placeholder="Select multiple countries"
+          options={suggestions}
+          components={components}
+          value={multi}
+          onChange={handleChangeMulti}
+          isMulti
+        />
+      </NoSsr>
+    </div>
+  )
+}
