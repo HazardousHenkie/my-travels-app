37f5339bcf7cd3b4d34368f5bd0ac0e1ddc5cce4
Kyle <khellenbrand@netwise.jp>
2019-08-31 17:50:52 +0900

    changing design

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Kyle @ -
+      <a href="https://www.freepik.com/free-photos-vectors/background">
+        Background vector created by freepik - www.freepik.com
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
