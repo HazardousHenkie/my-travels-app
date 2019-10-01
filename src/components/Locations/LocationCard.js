44c06f486679d736e6e67ac5d561a51c14527575
kyle <thekyle@outlook.com>
2019-10-01 23:12:52 +0900

    first commit to change responsive

--- src/components/Locations/LocationCard.js
+++ src/components/Locations/LocationCard.js
@@ -79,7 +79,7 @@ const LocationCard = ({ location, edit, firebase }) => {
   const avatarUserName = userNameCard.charAt(0)
 
   return (
-    <Grid item xs={4}>
+    <Grid item xs={12} sm={6} md={4}>
       <Card key={location.id}>
         {/* eslint-disable */}
         <CardHeader
