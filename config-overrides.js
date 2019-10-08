6c8cc297c54d304c7cca52f15dfa2c301af229e5
Kyle <thekyle@hotmail.com>
2019-10-08 23:20:17 +0900

    optimization

--- config-overrides.js
+++ config-overrides.js
@@ -3,9 +3,9 @@
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
 
 module.exports = function override(config, env) {
-  if (!config.plugins) {
-    config.plugins = []
-  }
-  config.plugins.push(new MomentLocalesPlugin())
+  //   if (!config.plugins) {
+  //     config.plugins = []
+  //   }
+  //   config.plugins.push(new MomentLocalesPlugin())
   return config
 }
